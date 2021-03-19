18606f1fd3ad8e91ed9d22ed2ddb79abfa2a857e
Aditya <adisawant100.as@gmail.com>
2021-03-20 02:50:47 +0530

    added cart icon at top

--- JS/shopInsertCard.js
+++ JS/shopInsertCard.js
@@ -1,13 +1,12 @@
 const grid = document.getElementById("card-grid");
 
 for(let i=0;i<6;i++){
-    grid.innerHTML +=  `<div class=" col-sm-12 col-md-4 justify-content-center"><div class='card  shop-cards' style="width: 15rem">
+    grid.innerHTML +=  `<div class=" col-sm-12 col-md-4 justify-content-center"><div class='card  shop-cards'>
     <img src="./../Images/chocolate_hamper.jpg" class="card-img-top"  alt="hamper image">
     <div class="card-body">
     <h5 class='card-title' id="card_title_`+i+`">Hamper `+i+`</h5>
     <p class="card-text">Description for the hamper</p>
-    <button class="btn btn-primary" id="addCart_`+i+`">Add to cart</button>
-    <span class="cart_counter_group" id="cart_counter_group_`+i+`"><button class="cart_counter_btn cart_counter_btn_left">+</button><span class="cart_counter" id="cart_counter_`+i+`">0</span><button class="cart_counter_btn_right cart_counter_btn">-</button></span>
+    <span class="cart_counter_group" id="cart_counter_group_`+i+`"><button class="cart_counter_btn cart_counter_btn_left" id="cartCountUp`+i+`">+</button><span class="cart_counter" id="cart_counter_`+i+`">0</span><button id="cartCountDown`+i+`" class="cart_counter_btn_right cart_counter_btn">-</button></span>
     </div></div>
     </div>`
     console.log("adding event")
@@ -15,10 +14,26 @@ for(let i=0;i<6;i++){
     const currentCart = "cart_counter_"+i
 }
 
+const cart = document.getElementById("cart")
 for(let i =0; i<6; i++){
-    document.getElementById("addCart_"+i).addEventListener("click",function(){
-        console.log("click")
+  
+    document.getElementById("cartCountUp"+i).addEventListener("click",function(){
         const counter = document.getElementById("cart_counter_"+i)
-        counter.innerHTML =  parseInt(counter.innerHTML) + 1
+        
+        if(parseInt(counter.innerHTML)<5){
+            counter.innerHTML =  parseInt(counter.innerHTML) + 1;
+            cart.innerText = parseInt(cart.innerText) + 1;
+        }
+        else{
+            alert("You can only add 5 items to the cart")
+        }
+        })
+    document.getElementById("cartCountDown"+i).addEventListener("click",function(){
+        const counter = document.getElementById("cart_counter_"+i)
+        if(parseInt(counter.innerHTML)>0){
+        counter.innerHTML =  parseInt(counter.innerHTML) - 1;
+        cart.innerText = parseInt(cart.innerText) - 1;
+
+        }
     })
 }
