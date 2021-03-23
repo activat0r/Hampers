fa0f1e8344401444dc438a3e6239645202ef4e3c
Aditya <adisawant100.as@gmail.com>
2021-03-23 06:41:12 +0530

    fixed shopping cards layout

--- JS/shopInsertCard.js
+++ JS/shopInsertCard.js
@@ -8,13 +8,15 @@ const storageCartJson = JSON.parse(storageCart)
 
 
 for(let i=0;i<6;i++){
-    grid.innerHTML +=  `<div class=" col-sm-12 col-md-4 justify-content-center"><div class='card  shop-cards'>
-    <img src="./Images/chocolate_hamper.jpg" class="card-img-top"  alt="hamper image">
-    <div class="card-body">
-    <h5 class='card-title' id="card_title_`+i+`">Hamper `+(i+1)+`</h5>
+    grid.innerHTML +=  `<div class=" col-sm-12 col-md-6 col-lg-4 justify-content-center"><div class='shop-cards'>
+    <img src="./Images/chocolate_hamper.jpg" class="cart-img"  alt="hamper image">
+    <div class="card-content">
+    <h5 class='' id="card_title_`+i+`">Hamper `+(i+1)+`</h5>
     <p class="card-text">Description for the hamper</p>
     <span class="cart_counter_group" id="cart_counter_group_`+i+`"><button class="cart_counter_btn cart_counter_btn_left" id="cartCountUp`+i+`">+</button><span class="cart_counter" id="cart_counter_`+i+`">0</span><button id="cartCountDown`+i+`" class="cart_counter_btn_right cart_counter_btn">-</button></span>
-    </div></div>
+
+    </div>
+    </div>
     </div>`
     console.log("adding event")
     const currentId = "addCart_"+i
